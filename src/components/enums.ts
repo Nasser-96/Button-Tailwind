d9733642e4747d20e6286fd31b5addcd7fec03ca
Nasser Abunasir <79524536+Nasser-96@users.noreply.github.com>
2024-02-06 15:53:12 +0300

    first

---
+++ src/components/enums.ts
@@ -0,0 +1,24 @@
+export enum ColorsEnum {
+  Primary = "primary",
+  Secondary = "secondary",
+  Danger = "danger",
+  Success = "success",
+  Warning = "warning",
+  Accent = "accent",
+  Muted = "muted",
+  White = "white",
+}
+export enum VariantsEnum {
+  Filled = "filled",
+  Outline = "outline",
+  Muted = "muted",
+  Link = "link",
+  None = "none",
+  White = "white",
+}
+
+export enum SizesEnum {
+  Small = "sm",
+  Medium = "md",
+  Large = "lg",
+}
