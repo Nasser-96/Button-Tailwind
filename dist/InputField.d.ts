ae19d20c0bf0a37292495f9a5f1ac3803be0d93a
Nasser Abunasir <79524536+Nasser-96@users.noreply.github.com>
2024-02-08 08:46:06 +0300

    delete input-field

--- dist/InputField.d.ts
+++
@@ -1,7 +0,0 @@
-interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    isValid?: boolean;
-    hint?: string;
-}
-declare function InputField({ hint, isValid, ...props }: InputFieldProps): JSX.Element;
-
-export { InputField };
